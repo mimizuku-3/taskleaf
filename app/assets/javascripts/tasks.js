4982b66ba7243775760f224637b65c13784990ff
mimizuku-3 <mizukipcmail@gmail.com>
2023-02-10 10:07:44 +0900

    update: delete tasks using Ajax

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,13 +2,11 @@
 // 『8-2-3: コントローラからJavaScriptを返して実行する』で不要になるためにコメントアウトする.
 //
 document.addEventListener('turbolinks:load', function() {
-  document.querySelectorAll('td').forEach(function(td) {
-    td.addEventListener('mouseover', function(e) {
-      e.currentTarget.style.backgroundColor = '#eff';
-    });
-
-    td.addEventListener('mouseout', function(e) {
-      e.currentTarget.style.backgroundColor = '';
+  document.querySelectorAll('.delete').forEach(function(a) {
+    a.addEventListener('ajax:success', function() {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = 'none';
     });
   });
 });
